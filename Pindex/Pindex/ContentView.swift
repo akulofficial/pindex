576da16d997bb88f892ed38bf814c985eb4e6509
Akul Gulrajani <gulrajani.2@osu.edu>
2019-09-29 16:12:47 -0400

    Create XCode project

---
+++ Pindex/Pindex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pindex
+//
+//  Created by Akul Gulrajani on 9/29/19.
+//  Copyright © 2019 Yeet. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
