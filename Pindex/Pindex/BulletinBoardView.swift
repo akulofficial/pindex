e48871abb3c460d550ea46f6f5a1a1b125ffcba6
Jake Taylor <>
2019-11-20 14:53:11 -0500

    paging Swetha and Jake

--- Pindex/Pindex/BulletinBoardView.swift
+++ Pindex/Pindex/BulletinBoardView.swift
@@ -7,6 +7,7 @@
 //
 
 import SwiftUI
+import FirebaseFirestore
 
 
 struct BulletinBoardView: View {
@@ -27,7 +28,10 @@ struct BulletinBoardView: View {
                 
             } // end of list
         }
-        .onAppear(perform: {self.getPosts()})
+        .onAppear(perform: {
+            //self.getPosts()
+            self.loadPostsWithPaging()
+        })
         .onDisappear(perform: {
             self.mapAction = 0 // resetting so that the user may tap the annotation again
             needToCenterLocation = true // resetting so that the map centers back on the user when the view swithces back
@@ -72,6 +76,73 @@ struct BulletinBoardView: View {
         }
         
     } // end of getPosts()
+    
+    
+    
+    
+    // called in place of getPosts()
+    func loadPostsWithPaging() {
+     
+        let numOfPosts:Int = getNumberOfPosts() // getting the number of posts for this bulletin board
+        var count = 0
+        let pagingAmount = 1
+        
+        while (count < 30) { // the current date has not been reached yet
+            getPostsPaging(startNum: count, endNum: (count + pagingAmount))
+            count += pagingAmount
+        } // end of while loop
+        
+    } // end of loadPostsWithPaging()
+    
+    // returns the number of posts associated with this bulletin board
+    func getNumberOfPosts() -> Int {
+        
+        var numOfPosts = 0
+        
+        db.collection("Post").whereField("ID", isEqualTo: currentBulletinBoard)
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents for bulletin post count: \(err)")
+                } else {
+                    
+                    for document in querySnapshot!.documents {
+                        numOfPosts += 1
+                    }
+                }
+                print("post size: \(self.posts.count)")
+        }
+        
+        return numOfPosts
+    
+    } // end of getNumberOfPosts()
+    
+    func getPostsPaging(startNum: Int, endNum: Int){
+        
+        var count:Int = startNum // used to create a specific id for posts
+        var postID:Int = count
+        
+        // looping through firestore to get all posts from this bulletin board and add them to the array above
+        db.collection("Post").whereField("ID", isEqualTo: currentBulletinBoard)
+            .whereField("PostNumber", isEqualTo: startNum)
+            .getDocuments()
+                { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting documents: \(err)")
+                } else {
+                    
+                    for document in querySnapshot!.documents {
+                        let documentDict = document.data()
+                        print("\(document.documentID) => \(documentDict)")
+                        let p = Post.init(title: documentDict["Title"] as! String, content: documentDict["Content"] as! String, id: postID)
+                        self.posts.append(p)
+                        postID += 1
+                    }
+                }
+                print("post size: \(self.posts.count)")
+                count += 1
+        }
+        
+    } // end of getPosts()
 
     
 } // end of BulletinBoardView
@@ -83,4 +154,18 @@ struct BulletinBoardView_Previews: PreviewProvider {
     }
 }
 */
+
+extension CollectionReference {
+    func whereField(_ field: String, isDateInToday value: Date) -> Query {
+        let components = Calendar.current.dateComponents([.year, .month, .day], from: value)
+        guard
+            let start = Calendar.current.date(from: components),
+            let end = Calendar.current.date(byAdding: .day, value: 1, to: start)
+        else {
+            fatalError("Could not find start date or calculate end date.")
+        }
+        return whereField(field, isGreaterThan: start).whereField(field, isLessThan: end)
+    }
+}
+
 
